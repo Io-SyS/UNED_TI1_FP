94bdf3a17a1e28def389d59d2117d6ef6aae36f2
Aurelio Baeza <iosys4@gmail.com>
2017-08-26 19:53:51 +0200

    Add files via upload

---
+++ practica_1.cpp
@@ -0,0 +1,19 @@
+/**************************************
+* NOMBRE: #Aurelio#
+* PRIMER APELLIDO: #Baeza#
+* SEGUNDO APELLIDO: #Romero#
+* DNI: #44582947#
+* EMAIL: #abaeza19@alumno.uned.es#
+**************************************/
+
+#include <stdio.h>
+
+int main(){
+
+  printf("Aurelio \n");
+  printf("Baeza \n");
+  printf("Romero \n");
+  printf("44582947 \n");
+  printf("abaeza19@alumno.uned.es \n");
+
+}
